d9ca3c94d9cfb644543743749d1a960e8a3dabcd
mstevenreyes <157348621+markstevenreyes@users.noreply.github.com>
2025-08-16 06:05:41 +0800

    added simple todo-list UI

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,7 +1,15 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import { useState, useRef } from 'react';
 
 export function Welcome() {
+  const [todoList, setTodoList] = useState< Array<{
+    id: number,
+    task: string
+  }>>([]);
+  const todoListId = useRef(1);
+  const selectedtodoItemId = useRef<number>(0)
+  
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
@@ -20,31 +28,52 @@ export function Welcome() {
           </div>
         </header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
+          <button type="button" onClick={AddTodoList} className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Add</button>
+          <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Delete</button>
           <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
+              To Do List App
             </p>
             <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
+                { todoList.length != 0 ? todoList.map((task, index) => (
+                  <li key={task.id}><input onChange={selectTodoItem} data-taskid={task.id} type="radio" name="task"/> task{task.id}</li>
+                )) : ""}
             </ul>
           </nav>
         </div>
       </div>
     </main>
   );
+
+  function AddTodoList (){
+    setTodoList([
+      ...todoList,
+      {
+        id: todoListId.current,
+        task: `task${todoListId.current}`
+      }
+    ]);
+    todoListId.current = todoListId.current + 1;
+  }
+
+  function selectTodoItem(e: any){
+    const todoItemId = Number(e.target.dataset['taskid'])
+    if(e.target.checked){
+      const newTodoList = todoList.filter(todoItem => todoItem.id !== todoItemId)
+    }
+  }
+
+  function deleteItem(e: any){
+    const todoItemId = Number(e.target.dataset['taskid'])
+    if(e.target.checked){
+      const newTodoList = todoList.filter(todoItem => todoItem.id !== todoItemId)
+      setTodoList([...newTodoList]);
+    }
+  }
+
+
 }
+
 
 const resources = [
   {
